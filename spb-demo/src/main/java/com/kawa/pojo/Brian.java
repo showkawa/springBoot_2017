0b4a42557fbfadf6ae95785ce333b12598d31524
Brian Huang <szhuangl@cn.ibm.com>
2018-09-27 16:29:27 +0800

    add yml

---
+++ spb-demo/src/main/java/com/kawa/pojo/Brian.java
@@ -0,0 +1,64 @@
+package com.kawa.pojo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ConfigurationProperties
+ *      告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定
+ */
+@Component
+@ConfigurationProperties(prefix = "brian")
+public class Brian {
+
+    private Date  kawadate;
+    private Map<String, Object> obj;
+    private List<String> lists;
+    private User user;
+
+    public Date getKawadate() {
+        return kawadate;
+    }
+
+    public void setKawadate(Date kawadate) {
+        this.kawadate = kawadate;
+    }
+
+    public Map<String, Object> getObj() {
+        return obj;
+    }
+
+    public void setObj(Map<String, Object> obj) {
+        this.obj = obj;
+    }
+
+    public List<String> getLists() {
+        return lists;
+    }
+
+    public void setLists(List<String> lists) {
+        this.lists = lists;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Brian{" +
+                "kawadate=" + kawadate +
+                ", obj=" + obj +
+                ", lists=" + lists +
+                ", user=" + user +
+                '}';
+    }
+}
--- spb-demo/src/test/java/com/kawa/spbdemo/SpbDemoApplicationTests.java
+++ spb-demo/src/test/java/com/kawa/spbdemo/SpbDemoApplicationTests.java
@@ -1,7 +1,9 @@
 package com.kawa.spbdemo;
 
+import com.kawa.pojo.Brian;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpbDemoApplicationTests {
 
+	@Autowired
+	Brian brian;
+
 	@Test
 	public void contextLoads() {
+		System.out.println(brian);
 	}
 
 }
