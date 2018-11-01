1be356ae5091cdd20ad9bcff70ef98f0d46c351b
Brian Huang <szhuangl@cn.ibm.com>
2018-11-01 14:18:11 +0800

    定时器任务-并行执行

---
+++ spb-demo/src/main/java/com/kawa/config/ScheduleConfig.java
@@ -0,0 +1,20 @@
+package com.kawa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executors;
+
+/**
+ *  设置线程池大小
+ *
+ *   newScheduledThreadPool 创建一个定长线程池，支持定时及周期性任务执行
+ */
+@Configuration
+public class ScheduleConfig implements SchedulingConfigurer {
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(Executors.newScheduledThreadPool(5));
+    }
+}
--- spb-demo/src/main/java/com/kawa/job/AsyncService.java
+++ spb-demo/src/main/java/com/kawa/job/AsyncService.java
@@ -2,13 +2,21 @@ package com.kawa.job;
 
 
 import com.kawa.mail.BrianMail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+
+/**
+ *
+ */
 @Service
 public class AsyncService {
+
+    private Logger logger = LoggerFactory.getLogger(AsyncService.class);
 
     @Autowired
     BrianMail brianMail;
@@ -21,11 +29,11 @@ public class AsyncService {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("异步任务");
+        logger.debug("异步任务");
     }
 
-    @Scheduled(cron = "0 * * * * *")
+    @Scheduled(cron = "0/5 * * * * *")
     public void brianScheduling() {
-        System.out.println("定时任务");
+        logger.debug("定时任务");
     }
 }
