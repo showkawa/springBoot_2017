78caa81471af9f262b18fa0a1cfdc0ca7931066d
1092249319@qq.com <1092249319@qq.com>
2021-06-14 15:19:22 +0800

    add the demo for spring cloud gateway

---
+++ spb-demo/spb-gateway/src/main/java/com/kawa/spbgateway/SpbGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kawa.spbgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbGatewayApplication.class, args);
+	}
+
+}
---
+++ spb-demo/spb-gateway/src/test/java/com/kawa/spbgateway/SpbGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kawa.spbgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class SpbGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
