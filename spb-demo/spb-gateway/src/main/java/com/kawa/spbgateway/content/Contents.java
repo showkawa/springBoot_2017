d4ed6512fe8983890f9aa666996d94acabdf9bc1
brian-root <1092249319@qq.com>
2021-07-25 16:36:55 +0800

    add a demo globalFilter

--- spb-demo/spb-gateway/src/main/java/com/kawa/spbgateway/content/Contents.java
+++ spb-demo/spb-gateway/src/main/java/com/kawa/spbgateway/content/Contents.java
@@ -1,8 +1,8 @@
 package com.kawa.spbgateway.content;
 
 public class Contents {
-    public final static String DEFAULT_PATH ="com.kawa.spbgateway.service";
-    public final static String GATEWAY_FILTER_PATH ="com.kawa.spbgateway.filter";
+    public final static String DEFAULT_PATH ="com.kawa.spbgateway.filter";
+    public final static String GATEWAY_FILTER_PATH ="com.kawa.spbgateway.service";
 
     /**
      * sort order
--- spb-demo/spb-gateway/src/main/java/com/kawa/spbgateway/filter/MaskFilter.java
+++ spb-demo/spb-gateway/src/main/java/com/kawa/spbgateway/filter/MaskFilter.java
@@ -1,18 +1,18 @@
 package com.kawa.spbgateway.filter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.filter.NettyWriteResponseFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.codec.multipart.Part;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
+import java.util.UUID;
 
 
 @Component
@@ -21,11 +21,10 @@ public class MaskFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        Mono<MultiValueMap<String, Part>> multipartData = exchange.getMultipartData();
-        Mono<MultiValueMap<String, String>> formData = exchange.getFormData();
         Map<String, Object> attributes = exchange.getAttributes();
         ServerHttpRequest request = exchange.getRequest();
-
+        MDC.put("gid", UUID.randomUUID().toString());
+        log.info("=== request ===:{}",request.getURI());
         return chain.filter(exchange.mutate().request(request).build());
     }
 
