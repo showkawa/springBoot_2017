2490a218cb4354286caad65a6fb7a05275c060a9
brian-root <1092249319@qq.com>
2021-07-30 06:14:08 +0800

    add a test for FileChannel

---
+++ spb-demo/spb-gateway/src/test/java/com/kawa/spbgateway/service/FileChannelTest.java
@@ -0,0 +1,32 @@
+package com.kawa.spbgateway.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+
+
+@Slf4j
+public class FileChannelTest {
+
+    @Test
+    public void When_GetDataFromFileChannel_Except_Success() throws IOException {
+        //reate the file input stream
+        var file = new File("/home/un/code/springBoot_2017/spb-demo/spb-gateway/src/main/resources/core.yml");
+        try(var fileInputStream = new FileInputStream(file)){
+           // get the FileChannel (FileChannelImpl) by FileInputStream
+           var fileChannel = fileInputStream.getChannel();
+           // create the buffer
+           var byteBuffer = ByteBuffer.allocate((int) file.length());
+           // read the channel data to buffer
+           fileChannel.read(byteBuffer);
+           log.info("=== core.yml ===: \r\n{}", new String(byteBuffer.array()));
+        }
+    }
+
+}
