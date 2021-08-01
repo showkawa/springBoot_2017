bd2380f8174dc76c7e20bc344481f7f952407376
brian-root <1092249319@qq.com>
2021-08-01 10:45:39 +0800

    add the test for nio channel

--- spb-demo/spb-gateway/src/test/java/com/kawa/spbgateway/service/FileChannelTest.java
+++ spb-demo/spb-gateway/src/test/java/com/kawa/spbgateway/service/FileChannelTest.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.UUID;
 
 
 @Slf4j
@@ -26,5 +28,31 @@ public class FileChannelTest {
             log.info("=== core.yml ===: \r\n{}", new String(byteBuffer.array()));
         }
     }
+
+    @Test
+    public void When_OutputData_Except_Success() throws IOException {
+        //create the file input stream
+        var file = new File("/home/un/code/springBoot_2017/spb-demo/spb-gateway/src/main/resources/core.yml");
+        ByteBuffer outputStr;
+
+        try (var fileInputStream = new FileInputStream(file)) {
+            // get the FileChannel (FileChannelImpl) by FileInputStream
+            var fileChannel = fileInputStream.getChannel();
+            // create the buffer
+            outputStr = ByteBuffer.allocate((int) file.length());
+            // read the channel data to buffer
+            fileChannel.read(outputStr);
+        }
+
+        // create a file output stream
+        var fileName = UUID.randomUUID().toString().replace("-", "");
+        try (var fileOutputStream = new FileOutputStream("/home/un/app/test/" + fileName)) {
+            // get file channel by stream
+            var channel = fileOutputStream.getChannel();
+            outputStr.flip();
+            channel.write(outputStr);
+        }
+    }
+
 
 }
