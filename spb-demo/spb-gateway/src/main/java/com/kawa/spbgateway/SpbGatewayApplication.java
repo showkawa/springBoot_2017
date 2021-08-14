2981c1bcc1658d2267ae7f6708241b73f0e8deb8
brian-root <1092249319@qq.com>
2021-08-14 09:30:25 +0800

    test the branch

--- spb-demo/spb-gateway/src/main/java/com/kawa/spbgateway/SpbGatewayApplication.java
+++ spb-demo/spb-gateway/src/main/java/com/kawa/spbgateway/SpbGatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpbGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpbGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpbGatewayApplication.class, args);
+    }
 
 }
