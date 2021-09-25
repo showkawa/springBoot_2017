f71c2a8e40f31dac68f2d57bc2b07f43f7f56dd8
brian-root <1092249319@qq.com>
2021-09-25 12:15:00 +0800

    update the ForkJoin demo

--- spb-demo/src/test/java/com/kawa/mutilthread/forkjoin/controller/ForkJoinTestController.java
+++ spb-demo/src/test/java/com/kawa/mutilthread/forkjoin/controller/ForkJoinTestController.java
@@ -1,7 +1,7 @@
 package com.kawa.mutilthread.forkjoin.controller;
 
 
-import com.brian.mutilthread.forkjoin.service.AutomationTask;
+import com.kawa.mutilthread.forkjoin.service.AutomationTask;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StopWatch;
