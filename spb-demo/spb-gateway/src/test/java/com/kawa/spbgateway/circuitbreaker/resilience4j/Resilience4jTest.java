0df37e57857d22fbb0939a89110eef0280e2bd24
brian-root <1092249319@qq.com>
2021-10-28 22:30:33 +0800

    add the test component contract-stub-runner

--- spb-demo/spb-gateway/src/test/java/com/kawa/spbgateway/circuitbreaker/resilience4j/Resilience4jTest.java
+++ spb-demo/spb-gateway/src/test/java/com/kawa/spbgateway/circuitbreaker/resilience4j/Resilience4jTest.java
@@ -1,24 +1,84 @@
 package com.kawa.spbgateway.circuitbreaker.resilience4j;
 
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.github.resilience4j.bulkhead.Bulkhead;
 import io.github.resilience4j.bulkhead.BulkheadConfig;
 import io.github.resilience4j.bulkhead.BulkheadRegistry;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.circuitbreaker.internal.InMemoryCircuitBreakerRegistry;
 import io.vavr.CheckedFunction0;
 import io.vavr.CheckedFunction1;
 import io.vavr.control.Try;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.cloud.gateway.support.TimeoutException;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.netty.http.client.HttpClient;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 @Slf4j
 public class Resilience4jTest {
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().port(8080));
+
+    private WebTestClient testClient;
+
+    private CircuitBreakerRegistry circuitBreakerRegistry;
+
+    private CircuitBreaker circuitBreaker;
+
+    private String PATH_200 = "/api/pancake/v1/coin/query";
+
+    private String PATH_400 = "/api/hk/card/v1/er/query";
+
+    private String PATH_500 = "/api/hk/card/v1/card/query";
+
+
+    @Before
+    public void setup() {
+        HttpClient httpClient = HttpClient.create().wiretap(true);
+        testClient = WebTestClient.bindToServer(new ReactorClientHttpConnector(httpClient))
+                .baseUrl("http://localhost:8080")
+                .responseTimeout(Duration.ofDays(1))
+                .build();
+
+        circuitBreakerRegistry = new InMemoryCircuitBreakerRegistry();
+        circuitBreaker = circuitBreakerRegistry.circuitBreaker("resilience4jTest",
+                CircuitBreakerConfig
+                        .custom()
+                        .failureRateThreshold(50)
+                        .minimumNumberOfCalls(10)
+                        .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                        .slidingWindowSize(10)
+                        .build());
+
+        Resilience4jTestHelper.circuitBreakerEventListener(circuitBreaker);
+
+        stubFor(post(urlMatching(PATH_200))
+                .willReturn(okJson("{}")));
+
+        stubFor(post(urlMatching(PATH_400))
+                .willReturn(badRequest()));
+
+        stubFor(post(urlMatching(PATH_500))
+                .willReturn(serverError()));
+
+
+    }
+
 
     @Test
     public void circuitBreakerRegistryTest() {
@@ -68,6 +128,31 @@ public class Resilience4jTest {
         CheckedFunction0<String> decorateCheckedSupplier = Bulkhead.decorateCheckedSupplier(bulkhead1, () -> "test the ");
         Try<String> result = Try.of(decorateCheckedSupplier).map(val -> val + "Bulkhead");
         log.info(">>>>>>>>>> bulkheadRegistryTest {} - {}", result.isSuccess(), result.get());
+
+    }
+
+    @Test
+    public void circuitBreakerTest() {
+        AtomicInteger count = new AtomicInteger();
+        try {
+            for (int i = 0; i < 10; i++) {
+                circuitBreaker.executeCheckedSupplier(() -> {
+                    Resilience4jTestHelper.recordResponseToCircuitBreaker(circuitBreaker, testClient, PATH_200);
+                    Resilience4jTestHelper.getCircuitBreakerStatus(">>>>>>>>>> end call " + count.incrementAndGet(), circuitBreaker);
+                    return null;
+                });
+            }
+            for (int i = 0; i < 10; i++) {
+                circuitBreaker.executeCheckedSupplier(() -> {
+                    Resilience4jTestHelper.recordResponseToCircuitBreaker(circuitBreaker, testClient, PATH_400);
+                    Resilience4jTestHelper.getCircuitBreakerStatus(">>>>>>>>>> end call " + count.incrementAndGet(), circuitBreaker);
+                    return null;
+                });
+            }
+        } catch (Throwable error) {
+            log.error(String.format(">>>>>>>>>> %s", error.getMessage()));
+        }
+
 
     }
 }
--- spb-demo/spb-gateway/src/test/java/com/kawa/spbgateway/circuitbreaker/resilience4j/Resilience4jTestHelper.java
+++ spb-demo/spb-gateway/src/test/java/com/kawa/spbgateway/circuitbreaker/resilience4j/Resilience4jTestHelper.java
@@ -2,13 +2,18 @@ package com.kawa.spbgateway.circuitbreaker.resilience4j;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Resilience4jTestHelper {
 
     /**
      * get the CircuitBreaker status and metrics
-     *      * @param prefixName
+     * * @param prefixName
+     *
      * @param circuitBreaker
      */
     public static void getCircuitBreakerStatus(String prefixName, CircuitBreaker circuitBreaker) {
@@ -24,8 +29,7 @@ public class Resilience4jTestHelper {
         int slowFailedCalls = metrics.getNumberOfSlowFailedCalls();
         int slowSuccessfulCalls = metrics.getNumberOfSlowSuccessfulCalls();
 
-        log.info(prefixName + "state=" + circuitBreaker.getState() + " , metrics[ failureRate=" + failureRate +
-                ", bufferedCalls=" + bufferedCalls +
+        log.info(prefixName + " state=" + circuitBreaker.getState() + " , metrics[ failureRate=" + failureRate +
                 ", failedCalls=" + failedCalls +
                 ", successCalls=" + successfulCalls +
                 ", notPermittedCalls=" + notPermittedCalls +
@@ -39,9 +43,57 @@ public class Resilience4jTestHelper {
 
     }
 
-//    public static void circuitBreakerEventListener(CircuitBreaker circuitBreaker){
-//        circuitBreaker.getEventPublisher().onEvent(event -> {
-//
-//        });
-//    }
+    public static void circuitBreakerEventListener(CircuitBreaker circuitBreaker) {
+        circuitBreaker.getEventPublisher()
+                .onSuccess(event -> {
+                    log.info(">>>>>>>>>> call service success:{}",event.toString());
+                })
+                .onError(event -> {
+                    log.info(">>>>>>>>>> call service failed:{}",event.toString());
+                })
+                .onIgnoredError(event -> {
+                    log.info(">>>>>>>>>> call service failed and ignore exception:{}",event.toString());
+                })
+                .onReset(event -> {
+                    log.info(">>>>>>>>>> circuit breaker reset:{}",event.toString());
+                })
+                .onStateTransition(event -> {
+                    log.info(">>>>>>>>>> circuit breaker status updated:{}",event.toString());
+                })
+                .onCallNotPermitted(event -> {
+                    log.info(">>>>>>>>>> circuit breaker opened:{}",event.toString());
+                })
+                .onFailureRateExceeded(event -> {
+                    log.info(">>>>>>>>>> exceeded failure rate:{}",event.toString());
+                })
+                .onSlowCallRateExceeded(event -> {
+                    log.info(">>>>>>>>>> exceeded slow call rate:{}",event.toString());
+                });
+    }
+
+    public static void recordResponseToCircuitBreaker(CircuitBreaker circuitBreaker, WebTestClient testClient, String path) {
+        WebTestClient.ResponseSpec responseSpec = testClient.post().uri(path).exchange();
+        try{
+//            responseSpec.expectStatus().is1xxInformational();
+            responseSpec.expectStatus().is2xxSuccessful();
+//            responseSpec.expectStatus().is3xxRedirection();
+            circuitBreaker.onSuccess(0, TimeUnit.MILLISECONDS);
+            return;
+        }catch (Throwable error){ }
+
+        try{
+            responseSpec.expectStatus().is4xxClientError();
+            circuitBreaker.onError(0,TimeUnit.MILLISECONDS,new Throwable("<<<< hit 4XX >>>>"));
+            return;
+        }catch (Throwable error){}
+
+        try{
+            responseSpec.expectStatus().is5xxServerError();
+            circuitBreaker.onError(0,TimeUnit.MILLISECONDS,new Throwable("<<<< hit 5XX >>>>"));
+        }catch (Throwable error){
+            circuitBreaker.onError(0,TimeUnit.MILLISECONDS,new Throwable("<<<< hit unknown error >>>>"));
+            return;
+        }
+
+    }
 }
