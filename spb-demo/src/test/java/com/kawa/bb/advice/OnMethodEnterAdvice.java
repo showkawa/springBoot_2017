97e35c554b4445460f58d799722052dab510073b
brian-root <1092249319@qq.com>
2022-03-26 17:17:45 +0800

    demo OnMethodEnter with skip condition by skipOn

--- spb-demo/src/test/java/com/kawa/bb/advice/OnMethodEnterAdvice.java
+++ spb-demo/src/test/java/com/kawa/bb/advice/OnMethodEnterAdvice.java
@@ -11,12 +11,10 @@ import net.bytebuddy.asm.Advice;
 public class OnMethodEnterAdvice {
 
     @Advice.OnMethodEnter(skipOn = Advice.OnDefaultValue.class)
-    public static int enter(@Advice.This Class<?> cls) {
-//    public static int enter() {
-        System.out.println("cls");
-        if (cls.getName().equals(OvUserService.class.getName())) {
-            System.out.println("1111111111");
-            return 0;
+    public static int enter(@Advice.This Object cls) {
+        if (cls.getClass().getName().equals(OvUserService.class.getName())) {
+            System.out.println(">>>>> enter class: " + cls);
+            return 1;
         }
         System.out.println("----- OnMethodEnterExitAdvice ----- enter");
         return 0;
